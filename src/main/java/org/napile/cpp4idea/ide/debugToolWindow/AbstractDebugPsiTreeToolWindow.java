c41be4e01d5a30d36ebb57517e29d3a679ee20f5
VISTALL <vistall.valeriy@gmail.com>
2013-07-17 22:49:04 +0300

    idea-project -> consulo-project

--- src/main/java/org/napile/cpp4idea/ide/debugToolWindow/AbstractDebugPsiTreeToolWindow.java
+++ src/main/java/org/napile/cpp4idea/ide/debugToolWindow/AbstractDebugPsiTreeToolWindow.java
@@ -16,12 +16,6 @@
 
 package org.napile.cpp4idea.ide.debugToolWindow;
 
-import java.awt.BorderLayout;
-
-import javax.swing.JPanel;
-
-import org.napile.cpp4idea.ide.debugToolWindow.psiDebug.XmlPsiDebugger;
-import org.napile.cpp4idea.lang.psiInitial.CPsiSharpFile;
 import com.intellij.ide.highlighter.XmlFileType;
 import com.intellij.openapi.Disposable;
 import com.intellij.openapi.application.Result;
@@ -36,13 +30,17 @@ import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiManager;
 import com.intellij.util.Alarm;
+import org.napile.cpp4idea.ide.debugToolWindow.psiDebug.XmlPsiDebugger;
+import org.napile.cpp4idea.lang.psiInitial.CPsiSharpFile;
+
+import javax.swing.*;
+import java.awt.*;
 
 /**
  * @author VISTALL
  * @date 15:54/29.12.12
  */
-public abstract class AbstractDebugPsiTreeToolWindow extends JPanel implements Disposable
-{
+public abstract class AbstractDebugPsiTreeToolWindow extends JPanel implements Disposable {
 	private static final int UPDATE_DELAY = 500;
 	protected static final String DEFAULT_TEXT =
 			"<!--\n" +
@@ -56,22 +54,18 @@ public abstract class AbstractDebugPsiTreeToolWindow extends JPanel implements D
 
 	protected XmlPsiDebugger debugger = new XmlPsiDebugger();
 
-	public AbstractDebugPsiTreeToolWindow(final Project project)
-	{
+	public AbstractDebugPsiTreeToolWindow(final Project project) {
 		super(new BorderLayout());
 		this.project = project;
 		editor = EditorFactory.getInstance().createEditor(EditorFactory.getInstance().createDocument(""), project, XmlFileType.INSTANCE, true);
 		add(editor.getComponent());
 		alarm = new Alarm(Alarm.ThreadToUse.SWING_THREAD, this);
-		alarm.addRequest(new Runnable()
-		{
+		alarm.addRequest(new Runnable() {
 			@Override
-			public void run()
-			{
+			public void run() {
 				alarm.addRequest(this, UPDATE_DELAY);
 				Location location = Location.fromEditor(FileEditorManager.getInstance(project).getSelectedTextEditor());
-				if(!Comparing.equal(location, currentLocation))
-				{
+				if (!Comparing.equal(location, currentLocation)) {
 					update(location, currentLocation);
 					currentLocation = location;
 				}
@@ -80,118 +74,101 @@ public abstract class AbstractDebugPsiTreeToolWindow extends JPanel implements D
 		}, UPDATE_DELAY);
 	}
 
-	private void update(Location location, Location oldLocation)
-	{
+	private void update(Location location, Location oldLocation) {
 		Editor editor = location.editor;
 
-		if(editor == null)
+		if (editor == null)
 			setText(DEFAULT_TEXT);
-		else
-		{
+		else {
 			VirtualFile vFile = ((EditorEx) editor).getVirtualFile();
-			if(vFile == null)
-			{
+			if (vFile == null) {
 				setText(DEFAULT_TEXT);
 				return;
 			}
 
 			PsiFile psiFile = PsiManager.getInstance(project).findFile(vFile);
-			if(!(psiFile instanceof CPsiSharpFile))
-			{
+			if (!(psiFile instanceof CPsiSharpFile)) {
 				setText(DEFAULT_TEXT);
 				return;
 			}
 
-			if(oldLocation == null || !Comparing.equal(oldLocation.editor, location.editor) || oldLocation.modificationStamp != location.modificationStamp)
+			if (oldLocation == null || !Comparing.equal(oldLocation.editor, location.editor) || oldLocation.modificationStamp != location.modificationStamp)
 				setText(toText(psiFile));
 		}
 	}
 
 	protected abstract String toText(PsiFile psiFile);
 
-	protected void setText(final String text)
-	{
-		new WriteCommandAction(project)
-		{
+	protected void setText(final String text) {
+		new WriteCommandAction(project) {
 			@Override
-			protected void run(Result result) throws Throwable
-			{
+			protected void run(Result result) throws Throwable {
 				editor.getDocument().setText(text);
 			}
 		}.execute();
 	}
 
 	@Override
-	public void dispose()
-	{
+	public void dispose() {
 		EditorFactory.getInstance().releaseEditor(editor);
 	}
 
-	public static class Location
-	{
+	public static class Location {
 		final Editor editor;
 		final long modificationStamp;
 		final int startOffset;
 		final int endOffset;
 
-		private Location(Editor editor)
-		{
+		private Location(Editor editor) {
 			this.editor = editor;
 			modificationStamp = editor != null ? editor.getDocument().getModificationStamp() : 0;
 			startOffset = editor != null ? editor.getSelectionModel().getSelectionStart() : 0;
 			endOffset = editor != null ? editor.getSelectionModel().getSelectionEnd() : 0;
 		}
 
-		public static Location fromEditor(Editor editor)
-		{
+		public static Location fromEditor(Editor editor) {
 			return new Location(editor);
 		}
 
-		public Editor getEditor()
-		{
+		public Editor getEditor() {
 			return editor;
 		}
 
-		public long getModificationStamp()
-		{
+		public long getModificationStamp() {
 			return modificationStamp;
 		}
 
-		public int getStartOffset()
-		{
+		public int getStartOffset() {
 			return startOffset;
 		}
 
-		public int getEndOffset()
-		{
+		public int getEndOffset() {
 			return endOffset;
 		}
 
 		@Override
-		public boolean equals(Object o)
-		{
-			if(this == o)
+		public boolean equals(Object o) {
+			if (this == o)
 				return true;
-			if(!(o instanceof Location))
+			if (!(o instanceof Location))
 				return false;
 
 			Location location = (Location) o;
 
-			if(endOffset != location.endOffset)
+			if (endOffset != location.endOffset)
 				return false;
-			if(modificationStamp != location.modificationStamp)
+			if (modificationStamp != location.modificationStamp)
 				return false;
-			if(startOffset != location.startOffset)
+			if (startOffset != location.startOffset)
 				return false;
-			if(editor != null ? !editor.equals(location.editor) : location.editor != null)
+			if (editor != null ? !editor.equals(location.editor) : location.editor != null)
 				return false;
 
 			return true;
 		}
 
 		@Override
-		public int hashCode()
-		{
+		public int hashCode() {
 			int result = editor != null ? editor.hashCode() : 0;
 			result = 31 * result + (int) (modificationStamp ^ (modificationStamp >>> 32));
 			result = 31 * result + startOffset;
