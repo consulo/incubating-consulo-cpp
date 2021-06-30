f2e4669fdd80ef95619c7072ec3a286598a37f9f
VISTALL <vistall.valeriy@gmail.com>
2021-06-30 21:30:00 +0300

    allow empty define

--- cpp-api/src/main/java/consulo/cpp/preprocessor/expand/ExpandedMacro.java
+++ cpp-api/src/main/java/consulo/cpp/preprocessor/expand/ExpandedMacro.java
@@ -3,8 +3,10 @@ package consulo.cpp.preprocessor.expand;
 import com.intellij.lang.ParserDefinition;
 import com.intellij.lexer.Lexer;
 import com.intellij.openapi.util.Pair;
+import com.intellij.psi.TokenType;
 import com.intellij.psi.tree.IElementType;
 import consulo.cpp.preprocessor.psi.CPreprocessorDefineDirective;
+import consulo.util.lang.StringUtil;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
@@ -14,7 +16,8 @@ import java.util.List;
  * @author VISTALL
  * @since 21:52/2020-07-31
  */
-public class ExpandedMacro {
+public class ExpandedMacro
+{
 	private final List<Pair<IElementType, String>> mySymbols = new ArrayList<>();
 
 	private final ParserDefinition myParserDefinition;
@@ -25,43 +28,59 @@ public class ExpandedMacro {
 
 	private final List<PreprocessorSymbolDoneInfo> mySymbolDoneInfos = new ArrayList<>();
 
-	public ExpandedMacro(ParserDefinition parserDefinition, CPreprocessorDefineDirective element, CharSequence text) {
+	public ExpandedMacro(ParserDefinition parserDefinition, CPreprocessorDefineDirective element, CharSequence text)
+	{
 		myParserDefinition = parserDefinition;
 		myElement = element;
 		myText = text;
 	}
 
-	public CPreprocessorDefineDirective getElement() {
+	public CPreprocessorDefineDirective getElement()
+	{
 		return myElement;
 	}
 
-	public void addSymbolDoneInfo(@NotNull PreprocessorSymbolDoneInfo doneInfo) {
+	public void addSymbolDoneInfo(@NotNull PreprocessorSymbolDoneInfo doneInfo)
+	{
 		mySymbolDoneInfos.add(doneInfo);
 	}
 
-	public List<PreprocessorSymbolDoneInfo> getSymbolDoneInfos() {
+	public List<PreprocessorSymbolDoneInfo> getSymbolDoneInfos()
+	{
 		return mySymbolDoneInfos;
 	}
 
-	public void expand() {
-		if (myExpanded) {
+	public void expand()
+	{
+		if(myExpanded)
+		{
 			return;
 		}
 
 		myExpanded = true;
 
-		Lexer lexer = myParserDefinition.createLexer(null);
-		lexer.start(myText);
+		if(!StringUtil.isEmpty(myText))
+		{
+			Lexer lexer = myParserDefinition.createLexer(null);
+			lexer.start(myText);
 
-		IElementType type;
-		while ((type = lexer.getTokenType()) != null) {
-			mySymbols.add(Pair.create(type, lexer.getTokenText()));
+			IElementType type;
+			while((type = lexer.getTokenType()) != null)
+			{
+				mySymbols.add(Pair.create(type, lexer.getTokenText()));
 
-			lexer.advance();
+				lexer.advance();
+			}
+		}
+
+		if(mySymbols.isEmpty())
+		{
+			mySymbols.add(Pair.create(TokenType.WHITE_SPACE, ""));
 		}
 	}
 
-	public List<Pair<IElementType, String>> getSymbols() {
+	public List<Pair<IElementType, String>> getSymbols()
+	{
 		return mySymbols;
 	}
 }
